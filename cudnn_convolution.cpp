180982d6144c6102cf04dc1a2741998fabfae188
Jordan Guerguiev <jordan.guerguiev@me.com>
2019-07-26 18:28:06 -0400

    Initial commit

---
+++ cudnn_convolution.cpp
@@ -0,0 +1,96 @@
+#include <torch/extension.h>
+#include <vector>
+#include <ATen/NativeFunctions.h>
+#include <ATen/Config.h>
+
+/*
+PyTorch extension enabling direct access to the following cuDNN-accelerated C++ functions
+that are included in PyTorch:
+
+    - cudnn_convolution
+    - cudnn_convolution_backward_weight
+    - cudnn_convolution_backward_input
+
+The functions defined here can be called from Python in replacement of
+torch.nn.conv2d, torch.nn.grad.conv2d_weight and torch.nn.grad.conv2d_input,
+and run significantly faster. See 'example.py' for how these functions
+are called.
+
+Adapted from code posted by hanspinckaers:
+https://discuss.pytorch.org/t/cuda-error-with-cudnn-convolution-backward-weight-function/41214
+*/
+
+at::Tensor convolution(
+    const at::Tensor& input,
+    const at::Tensor& weight,
+    const at::Tensor& bias,
+    c10::ArrayRef<int64_t> stride,
+    c10::ArrayRef<int64_t> padding,
+    c10::ArrayRef<int64_t> dilation,
+    int64_t groups,
+    bool benchmark,
+    bool deterministic) {
+
+    return at::cudnn_convolution(
+        input,
+        weight,
+        bias,
+        padding,
+        stride,
+        dilation,
+        groups,
+        benchmark,
+        deterministic);
+}
+
+at::Tensor convolution_backward_weight(
+    const at::Tensor& input,
+    c10::ArrayRef<int64_t> weight_size,
+    const at::Tensor& grad_output,
+    c10::ArrayRef<int64_t> stride,
+    c10::ArrayRef<int64_t> padding,
+    c10::ArrayRef<int64_t> dilation,
+    int64_t groups,
+    bool benchmark,
+    bool deterministic) {
+
+    return at::cudnn_convolution_backward_weight(
+        weight_size,
+        grad_output,
+        input,
+        padding,
+        stride,
+        dilation,
+        groups,
+        benchmark,
+        deterministic);
+}
+
+at::Tensor convolution_backward_input(
+    c10::ArrayRef<int64_t> input_size,
+    const at::Tensor& weight,
+    const at::Tensor& grad_output,
+    c10::ArrayRef<int64_t> stride,
+    c10::ArrayRef<int64_t> padding,
+    c10::ArrayRef<int64_t> dilation,
+    int64_t groups,
+    bool benchmark,
+    bool deterministic) {
+
+    return at::cudnn_convolution_backward_input(
+        input_size,
+        grad_output,
+        weight,
+        padding,
+        stride,
+        dilation,
+        groups,
+        benchmark,
+        deterministic);
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("convolution", &convolution, "convolution");
+    m.def("convolution_backward_weight", &convolution_backward_weight, "convolution backward weight");
+    m.def("convolution_backward_input", &convolution_backward_input, "convolution backward input");
+}
---
+++ example.py
@@ -0,0 +1,28 @@
+import torch
+from torch.utils.cpp_extension import load
+
+# load the PyTorch extension
+cudnn_convolution = load(name="cudnn_convolution", sources=["cudnn_convolution.cpp"], verbose=True)
+
+# create dummy input, convolutional weights and bias
+input  = torch.zeros(128, 3, 32, 32).to('cuda')
+weight = torch.zeros(64, 3, 5, 5).to('cuda')
+bias   = torch.zeros(64).to('cuda')
+
+stride   = (2, 2)
+padding  = (0, 0)
+dilation = (1, 1)
+groups   = 1
+
+# compute the result of convolution
+output = cudnn_convolution.convolution(input, weight, bias, stride, padding, dilation, groups, False, False)
+
+# create dummy gradient w.r.t. the output
+grad_output = torch.zeros(128, 64, 14, 14).to('cuda')
+
+# compute the gradient w.r.t. the weights and input
+grad_weight = cudnn_convolution.convolution_backward_weight(input, weight.shape, grad_output, stride, padding, dilation, groups, False, False)
+grad_input  = cudnn_convolution.convolution_backward_input(input.shape, weight, grad_output, stride, padding, dilation, groups, False, False)
+
+print(grad_weight.shape)
+print(grad_input.shape)
